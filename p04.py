aa7f7b926082cd9f3e559ceb70d3cac5bbf317bf
luis <luis.alberto.hernandez.cozatl@uabc.edu.mx>
2020-03-27 20:32:32 -0700

    Realizacion de las interfaz(registrar y eliminar) y vinculacion con el menu principal

--- p04.py
+++ p04.py
@@ -78,14 +78,53 @@ def inventario():
 	labelProducto.place(x=200,y=100)
 
 	#este boton te redirigira a otra ventana
-	Button(inv, text='AGREGAR PRODUCTOS', command=inv.destroy).place(x=100,y=300)
+	Button(inv, text='AGREGAR PRODUCTOS', command=partial(venAgregarP,inv)).place(x=100,y=300)
 	#este boton te redirigira a otra ventana
 	#ejemplo de como redirigirse a otra ventana con un botton
 	#el codigo de aqui abajo
 	Button(inv, text='EDITAR PRODUCTOS', command=partial(venEditar,inv)).place(x=250,y=300)
 	#este boton te redirigira a otra ventana
-	Button(inv, text='ELIMINAR PRODUCTOS', command=inv.destroy).place(x=400,y=300)
+	Button(inv, text='ELIMINAR PRODUCTOS', command=partial(venBorrarP,inv)).place(x=400,y=300)
 	
+#Funcion Agregar Poductos
+def venAgregarP(inv):	
+	inv.withdraw()
+	agregarWin=Tk()
+	agregarWin.title("REGISTRAR PRODUCTOS")
+	agregarWin.resizable(0,0)
+	agregarWin.geometry("640x480")
+	#label de encabezado
+	labelEncabezado = Label(agregarWin,text="REGISTRO DE PRODUCTOS")
+	labelEncabezado.grid(row=0,column=0,sticky="NW")
+	labelEncabezado.place(x=250,y=10)
+	#label de usuario para Producto
+	labelCodigo=Label(agregarWin,text="Codigo:")
+	labelCodigo.grid(row=0,column=0,sticky="e",padx=20, pady=100)
+	cuadroCodigo=Entry(agregarWin,textvariable="Code")
+	cuadroCodigo.grid(row=0,column=1, sticky="e",padx=20, pady=100)
+
+	#label de usuario para Precio
+	labelNombre=Label(agregarWin,text="Nombre:")
+	labelNombre.grid(row=0,column=2,sticky="e",padx=1, pady=5)
+	cuadroNombre=Entry(agregarWin,textvariable="User")
+	cuadroNombre.grid(row=0,column=3, sticky="e",padx=30, pady=5)
+
+	#label de usuario para cantidad
+	labelCantidad=Label(agregarWin,text="Cantidad:")
+	labelCantidad.grid(row=1,column=0,sticky="e",padx=15, pady=5)
+	cuadroCantidad=Entry(agregarWin,textvariable="Cant")
+	cuadroCantidad.grid(row=1,column=1, sticky="e",padx=15, pady=5)
+
+	#label de usuario para Codigo
+	labelPrecio=Label(agregarWin,text="Precio:")
+	labelPrecio.grid(row=1,column=2,sticky="e",padx=20, pady=5)
+	cuadroPrecio=Entry(agregarWin,textvariable="Costo")
+	cuadroPrecio.grid(row=1,column=3, sticky="e",padx=15, pady=5)
+
+	Button(agregarWin, text='AGREGAR', command=agregarWin.destroy).place(x=150,y=300)
+	
+	Button(agregarWin, text='REGRESAR', command=partial(regresar,agregarWin)).place(x=350,y=300)
+
 	
 	
 	
@@ -115,5 +154,25 @@ def venEditar(inv):
 	Button(editarWin, text='REGRESAR', command=partial(regresar,editarWin)).place(x=250,y=300)
 	#este boton te redirigira a otra ventana
 	
+def venBorrarP(inv):
+	inv.withdraw()
+	borrarWin=Tk()
+	borrarWin.title("ELIMINAR PRODUCTOS")
+	borrarWin.resizable(0,0)
+	borrarWin.geometry("640x480")
+	raiz.config(backgroun="grey",padx=15, pady=80)
+	labelEncabezado = Label(borrarWin,text="ELIMINAR PRODUCTO")
+	labelEncabezado.grid(row=0,column=0,sticky="NW")
+	labelEncabezado.place(x=250,y=10)
+
+	labelEliminar=Label(borrarWin,text="Codigo Del Producto:")
+	labelEliminar.grid(row=0,column=0,sticky="e",padx=80, pady=150)
+	cuadroEliminar=Entry(borrarWin,text="Code")
+	cuadroEliminar.grid(row=0,column=1, sticky="e",padx=30, pady=160)
+
+	Button(borrarWin, text='ELIMINAR', command=borrarWin.destroy).place(x=210,y=300)
+	
+	Button(borrarWin, text='REGRESAR', command=partial(regresar,borrarWin)).place(x=290,y=300)
+
 		
 raiz.mainloop()
