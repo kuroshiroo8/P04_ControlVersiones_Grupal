a65e9195cc9aa9147b924ec466b9acee56a567e8
Rimpler <esmir.sanchez@uabc.edu.mx>
2020-04-01 19:21:34 -0700

    se agrega eliminar y editar mas correciones de bottones

--- p04.py
+++ p04.py
@@ -125,7 +125,7 @@ def venRegistrar():
     btguardar = tk.Button(RegistrarWin, text =  "GUARDAR", fg="blue",font=("arial", 12),cursor = "hand2",relief = "raised",command = guarda)
     btguardar.pack()
     btguardar.place(x=300,y=350)
-    
+
 ########################interfaz para ver los productos
 def venMostrar():
     #windows.withdraw()
@@ -269,6 +269,206 @@ def venEditar():
     bt_modificar.place(x=280,y=350)
 
 
+########################interfaz para ver agregar mermas
+#Funcion Agregar Mermas
+def venRegistrarM():
+    #windows.withdraw()
+ 
+    RegistrarWinM=tk.Toplevel()
+    RegistrarWinM.title("RegistrarWinM")
+    RegistrarWinM.resizable(0,0)
+    RegistrarWinM.iconbitmap("13.ico")
+    RegistrarWinM.geometry("550x550")
+
+    e1=tk.Label(RegistrarWinM, text="AGREGAR MERMAS :",bg="white",fg="black").place(x=50, y=20)
+ 
+# variable codigo
+    entrycodigo=tk.StringVar()
+    codigotx=tk.Entry(RegistrarWinM,textvariable=entrycodigo).place(x=40, y=120)
+   
+# variable producto
+    entryproducto=tk.StringVar()
+    productotx=tk.Entry(RegistrarWinM,textvariable=entryproducto).place(x=40, y=170)
+    
+# variable cantidad
+    entrycantidad=tk.StringVar()
+    cantidadtx=tk.Entry(RegistrarWinM,textvariable=entrycantidad).place(x=40, y=220)
+    
+# Etiqueta para "INGRESE CODIGO DEL PRODUCTO"
+    etiquetacodigo=tk.Label(RegistrarWinM, text="INGRESE CODIGO DE MERMA.", padx=10 ).place(x=30, y=100)
+    
+# Etiqueta para "INGRESE NOMBRE DEL PRODUCTO"
+    etiquetanombre=tk.Label(RegistrarWinM, text="INGRESE NOMBRE DE MARMA.", padx=10 ).place(x=30, y=150)
+
+# Etiqueta para "INGRESE NOMBRE DEL CANTIDAD"
+    etiquetacantidad=tk.Label(RegistrarWinM, text="INGRESE CANTIDAD DE MERMA.", padx=10 ).place(x=30, y=200)
+   
+
+   ## Boton menu  
+
+    menu=tk.Button(RegistrarWinM, text="MENU", fg="red",font=("arial", 12),cursor = "hand2",relief = "raised",command = RegistrarWinM.destroy)
+    menu.pack()
+    menu.place(x=50,y=350)
+
+    def guardaM():
+
+            db = sqlite3.connect("articulos.db")
+            c = db.cursor()
+
+            codigo = entrycodigo.get()
+            nombre = entryproducto.get()
+            cantidad = entrycantidad.get()
+          
+
+            c.execute("insert into tbMerma (codigo,nombre,cantidad) values ('"+codigo+"','"+nombre+"','"+cantidad+"')")
+            db.commit()
+            c.close()
+            messagebox.showinfo("MODIFICACION","MERMA INGRESADA" )
+            RegistrarWinM.destroy()
+            venRegistrarM()
+
+    btguardar = tk.Button(RegistrarWinM, text =  "GUARDAR", fg="blue",font=("arial", 12),cursor = "hand2",relief = "raised",command = guardaM)
+    btguardar.pack()
+    btguardar.place(x=300,y=350)    
+
+
+########################interfaz para ver las mermas
+def venMostrarM():
+    #windows.withdraw()
+    MostrarWinM=tk.Toplevel()
+    MostrarWinM.geometry("640x480")
+    e1=tk.Label(MostrarWinM, text="VER MERMAS :",bg="white",fg="black").place(x=50, y=50),
+    lbl_code=tk.Label(MostrarWinM, text="CODIGO",bg="white",fg="black").place(x=50, y=70)
+    lbl_name=tk.Label(MostrarWinM, text="NOMBRE",bg="white",fg="black").place(x=110, y=70)
+    lbl_cant=tk.Label(MostrarWinM, text="CANTIDAD",bg="white",fg="black").place(x=180, y=70)
+
+  ###########consulta para mostrar los productos de la base
+    def mostrarM():
+##        codigo
+        lista=tk.Listbox(MostrarWinM, width = 50, font=("arial", 12), height =15 )
+        lista.pack()
+        db = sqlite3.connect("articulos.db")
+        c = db.cursor()
+        c.execute( "select *from tbMerma  ORDER BY (ID)DESC" )
+        for row in c:
+            lista.insert(0,str(row[1])+" --------"+ row[2]+"--------"+ str(row[3]))
+            lista.place(x=50,y=90)
+
+
+    menu=tk.Button(MostrarWinM, text="MENU", fg="red",command = MostrarWinM.destroy)
+    menu.pack()
+    menu.place(x=50,y=400)
+
+    bt_mostrar = tk.Button(MostrarWinM, text =  "MOSTRAR MERMAS", fg="green",command = mostrarM)
+    bt_mostrar.pack()
+    bt_mostrar.place(x=280,y=400)
+
+#####################################################################################################################
+
+def venBorrarM():
+    EliminarWinM=tk.Toplevel()
+    EliminarWinM.title("ELIMINAR")
+    EliminarWinM.resizable(0,0)
+    EliminarWinM.iconbitmap("14.ico")
+    EliminarWinM.geometry("512x512")
+    e1=tk.Label(EliminarWinM, text=" ELIMINAR MERMA :",bg="white",fg="black").place(x=50, y=50)
+    
+#### VARIABLE PARA ID
+    
+    entry_id=tk.StringVar()
+    productotx=tk.Entry(EliminarWinM,textvariable=entry_id).place(x=50, y=150)
+
+#### ETIQUETA PARA ID
+
+    etiquetanombre=tk.Label(EliminarWinM, text="INGRESE CODIGO DE MERMA", padx=10 ).place(x=30, y=115)
+#
+    def eliminarM():
+        db = sqlite3.connect("articulos.db")
+        c = db.cursor()
+
+        
+        codigo_merma = entry_id.get()
+        c.execute("DELETE  from tbMerma where codigo = ('"+codigo_merma+"')")
+        messagebox.showinfo("MODIFICACION","MERMA ELIMINADA" )
+
+        db.commit()
+        c.close()
+        EliminarWinM.destroy()
+        venBorrarM()
+        
+#### BOTON PARA MENU
+    
+    menu=tk.Button(EliminarWinM, text="MENU", fg="red",font=("arial", 12),cursor = "hand2",relief = "raised",command = EliminarWinM.destroy)
+    menu.pack()
+    menu.place(x=50,y=350)
+#### BOTON PARA ELIMINAR
+    bt_eliminar = tk.Button(EliminarWinM, text =  "ELIMINAR MERMA", fg="blue",font=("arial", 12),cursor = "hand2",relief = "raised",command = eliminarM)
+    bt_eliminar.pack()
+    bt_eliminar.place(x=280,y=350)
+     
+ ###################### VENTANA PARA MODIFICAR
+
+def venEditarM():
+    #windows.withdraw()
+    editarWinM=tk.Toplevel()
+    editarWinM.title("MODIFICAR MERMA")
+    editarWinM.resizable(0,0)
+   
+    editarWinM.geometry("640x480")
+    lbl_Encabezado=tk.Label(editarWinM, text=" MODIFICAR MERMA :",bg="white",fg="black").place(x=50, y=50)
+    
+#texbox para id
+    
+    ProctID=tk.StringVar()
+    cuadroID=tk.Entry(editarWinM,textvariable=ProctID).place(x=50, y=120)
+
+#textbox para codigo
+
+    lblcode=tk.Label(editarWinM, text="INGRESE CODIGO DE MERMA", padx=10 ).place(x=30, y=100)
+
+#txtxbox para nombre
+    Proctname=tk.StringVar()
+    cuadroVx=tk.Entry(editarWinM,textvariable=Proctname).place(x=50, y=170)
+#textbox para la cantidad
+    
+    Proctncand=tk.StringVar()
+    cuadroVx=tk.Entry(editarWinM,textvariable=Proctncand).place(x=50, y=230)
+
+###############################################################
+
+    #labels para los valores a modificar
+
+    lblname=tk.Label(editarWinM, text="INGRESE EL NUEVO NOMBRE PARA LA MERMA", padx=10 ).place(x=30, y=150)
+    lblcant=tk.Label(editarWinM, text="INGRESE LA NUEVA CANTIDAD PARA LA MERMA", padx=10 ).place(x=30, y=200)
+    
+  ################modificar el producto consulta
+    def modificarM():
+        db = sqlite3.connect("articulos.db")
+        c = db.cursor()
+
+        codigo_merma = ProctID.get()
+        nuevo_no = Proctname.get()
+        nuevo_ca = Proctncand.get()
+    
+        c.execute("update tbMerma set (nombre,cantidad) =('"+nuevo_no+"','"+nuevo_ca+"') where codigo = ('"+codigo_merma+"')")
+        db.commit()
+        c.close()
+        messagebox.showinfo("MODIFICACION","MERMA MODIFICADA" )
+        editarWinM.destroy()
+        venEditarM()
+
+#### BOTON PARA MENU
+    
+    menu=tk.Button(editarWinM, text="MENU", fg="red",command = editarWinM.destroy)
+    menu.pack()
+    menu.place(x=50,y=350)
+#### BOTON PARA MODIFICAR
+    bt_modificar = tk.Button(editarWinM, text =  "MODIFICAR MERMA", fg="green",command = modificarM)
+    bt_modificar.pack()
+    bt_modificar.place(x=280,y=350)    
+
+##############################################################################################################################
+
 
 ###################funcion de la interfaz de inventario
 def principal():
@@ -300,5 +500,21 @@ def principal():
 	bt_edit=tk.Button(inv,text="Modificar Producto",  command = venEditar)
 	bt_edit.pack()
 	bt_edit.place(x=5,y=350)
+	#boton para dirigirse a la interfaz de agregar mermas
+	bt_edit=tk.Button(inv,text="Agregar Mermas",  command = venRegistrarM)
+	bt_edit.pack()
+	bt_edit.place(x=250,y=50)
+	#boton para dirigirse a la interfaz de mostrar mermas
+	bt_edit=tk.Button(inv,text="Mostrar Mermas",  command = venMostrarM)
+	bt_edit.pack()
+	bt_edit.place(x=250,y=150)
+	#boton para dirigirse a la interfaz de eliminar mermas
+	bt_edit=tk.Button(inv,text="Eliminar Mermas",  command = venBorrarM)
+	bt_edit.pack()
+	bt_edit.place(x=250,y=250)
+	#boton para dirigirse a la interfaz de editar mermas
+	bt_edit=tk.Button(inv,text="Editar Mermas",  command = venEditarM)
+	bt_edit.pack()
+	bt_edit.place(x=250,y=350)
 		
 raiz.mainloop()
